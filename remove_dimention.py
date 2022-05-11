9964bc0ae60be94a584be946b2bc2b1e01bc339a
user <yardenge@edu.hac.ac.il>
2022-05-11 13:38:46 +0300

    ex12

---
+++ remove_dimention.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def remove_dimension(shape):
+    """
+    This function removes single-dimensional entries from a specified shape.
+    :return: The given shape without single-dimensional entries
+    """
+    return np.squeeze(shape).shape
+
+
+if __name__ == "__main__":
+    print(remove_dimension([1, 2, 3, 4, 5, 6]))
