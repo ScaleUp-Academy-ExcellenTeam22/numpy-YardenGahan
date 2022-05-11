6f0441641a6de991688ab8711c412159389d1e79
user <yardenge@edu.hac.ac.il>
2022-05-11 13:54:46 +0300

    ex14

--- combine_array.py
+++ combine_array.py
@@ -2,9 +2,15 @@ import numpy as np
 
 
 def combine_array(oned, twod):
+    """
+    This function combines a one and a two dimensional array together and display their elements.
+    :param oned:  one dimensional array
+    :param twod:  two dimensional array
+    :return:
+    """
     for x, y in np.nditer([oned, twod]):
-        print("%d:%d" % (x, y),)
+        print(f" {x} : {y}")
 
 
 if __name__ == "__main__":
-    print(combine_array([1, 2, 3], ([4, 5, 6], [7, 8, 9])))
+    combine_array([1, 2, 3], ([4, 5, 6], [7, 8, 9]))
