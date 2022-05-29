315dd5f33e3df690047fba0ee5d474a4ef153c44
YardenGahan <74112018+YardenGahan@users.noreply.github.com>
2022-05-29 10:20:00 +0300

    changes function

--- unit_matrix.py
+++ unit_matrix.py
@@ -6,7 +6,7 @@ def unit_metrix():
     This function creates a 3-D array with ones on a diagonal and zeros elsewhere.
     :return: 3-D array with ones on a diagonal and zeros elsewhere.
     """
-    return np.eye(3)
+    return np.identity(3)
 
 
 if __name__ == "__main__":
