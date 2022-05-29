4979da9d5537539689d3b0d50b41f11dae4b40b1
YardenGahan <74112018+YardenGahan@users.noreply.github.com>
2022-05-29 10:18:30 +0300

    fixed argument name

--- remove_dimention.py
+++ remove_dimention.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def remove_dimension(shape):
+def remove_dimension(matrix):
     """
-    This function removes single-dimensional entries from a specified shape.
-    @:arg shape: array or list
-    :return: The given shape without single-dimensional entries
+    This function removes single-dimensional entries from a given matrix.
+    @:arg matrix: a matrix
+    :return: The given matrix without single-dimensional entries
     """
     return np.squeeze(shape).shape
 
